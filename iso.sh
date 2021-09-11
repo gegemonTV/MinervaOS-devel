4f79e8e5c962443eb535c26bb2919cf4b92d4c2b
Vladimir <51519538+gegemonTV@users.noreply.github.com>
2021-09-11 13:19:23 +0300

    updated OS name

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/minervaos.kernel isodir/boot/minervaos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "minervaos" {
+	multiboot /boot/minervaos.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o minervaos.iso isodir
